72a98b3817a06db84c0c062a57bdab17aa20e9bd
Fabiola Aguilera <fabiaguilera96@gmail.com>
2022-03-22 19:53:09 -0400

    added Delete methods in Services and Controller; added Delete view

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -93,6 +93,28 @@ namespace ElevenNote.WebMVC.Controllers
             return View(model);
         }
 
+        //GET: DELETE
+        [ActionName("Delete")]
+        public ActionResult Delete(int id)
+        {
+            var svc = CreateNoteService();
+            var model = svc.GetNoteById(id);
+            
+            return View(model);
+        }
+
+        // POST:DELETE
+        [HttpPost]
+        [ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeletePost (int id)
+        {
+            var service = CreateNoteService();
+            service.DeleteNote(id);
+            TempData["SaveResult"] = "Your note was deleted.";
+            return RedirectToAction("Index");
+        }
+
         private NoteService CreateNoteService()
         {
             var userId = Guid.Parse(User.Identity.GetUserId());
